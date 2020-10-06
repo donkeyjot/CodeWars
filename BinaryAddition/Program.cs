a2d82e44a909b801df8bf9b160657191fc64d4f5
Ondrej Sebl <62696517+donkeyjot@users.noreply.github.com>
2020-10-06 11:29:38 +0200

    remember there is Convert class with function Convert.String
    asd
    sdg
    q
    esc
    quit
    als
    zxc
    g
    sf
    h
    
    .
    /
    ,
    ;
    '
    
    '
    ][
    
    ][da
    
    zf
    
    c
    v

---
+++ BinaryAddition/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BinaryAddition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddBinary(4,3));
+        }
+
+        public static string AddBinary(int a, int b)
+        {
+            int sum = a + b;
+
+
+            return ReverseString(ToBinary(sum));
+
+        }
+
+        public static string ToBinary(int dec)
+        {
+            if (dec == 1)
+            {
+                return "1";
+            }
+            else
+            {
+                return dec % 2 + ToBinary(dec / 2);
+            }
+        }
+        public static string ReverseString(string originial)
+        {
+            char[] array = originial.ToCharArray();
+            Array.Reverse( array );
+            return new string(array);
+
+        }
+    }
+}
