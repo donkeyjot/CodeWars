81c3391a9199863315daaf3796ff65f721fdd735
Ondrej Sebl <62696517+donkeyjot@users.noreply.github.com>
2020-10-06 12:04:21 +0200

    two solutions ok. I need to use usings on CW

---
+++ SumOfTwoLowestPositiveIntegers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumOfTwoLowestPositiveIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        public static int SumTwoSmallestNumbers(int[] numbers)
+        {
+            int[] sorted = numbers;
+            Array.Sort(sorted);
+            return sorted[0] + sorted[1];
+        }
+
+        public static int sumTwoSmallestNumbers(int[] numbers)
+        {
+            List<int> numbersList = new List<int>();
+            foreach (var number in numbers)
+            {
+                numbersList.Add(number);
+            }
+
+            int smallest = numbersList.Min();
+            numbersList.Remove(smallest);
+            int secondSmallest = numbersList.Min();
+
+            return smallest + secondSmallest;
+        }
+    }
+}
